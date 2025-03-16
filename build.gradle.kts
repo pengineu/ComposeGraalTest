fa34448556cc435b9e9e4430d834f8d267fb3514
Gorasq <successorgood@gmail.com>
2025-03-16 23:46:43 +0900

    initial commit
    hello world

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm")
+    kotlin("multiplatform") version "2.1.10"
+    id("org.jetbrains.compose")
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("com.github.johnrengelman.shadow").version("7.1.2")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    // Note, if you develop a library, you should use compose.desktop.common.
+    // compose.desktop.currentOs should be used in launcher-sourceSet
+    // (in a separate module for demo project and in testMain).
+    // With compose.desktop.common you will also lose @Preview functionality
+    implementation(compose.desktop.currentOs)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "ComposeGraalTest"
+            packageVersion = "1.0.0"
+        }
+    }
+}
+
+tasks.shadowJar {
+    manifest {
+        attributes(mapOf("Main-Class" to "MainKt"))
+    }
+}
+
+//tasks {
+//    register<Jar>("uberJar") {
+//        archiveClassifier.set("uber")
+//
+//        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+//
+//        manifest {
+//            attributes["Main-Class"] = "MainKt"
+//        }
+//
+//        from(sourceSets.main.get().output)
+//
+//        dependsOn(configurations.runtimeClasspath)
+//        from({
+//            configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+//        })
+//    }
+//}
